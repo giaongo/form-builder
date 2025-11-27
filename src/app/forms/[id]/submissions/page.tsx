ae81f5045aef9b64de42c6cb8b7863642c460702
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2025-11-28 03:11:55 +0530

    chore: remove auth checks from protected pages

--- src/app/forms/[id]/submissions/page.tsx
+++ src/app/forms/[id]/submissions/page.tsx
@@ -1,9 +1,5 @@
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
 import SubmissionsPage from '@/components/SubmissionsPage'
 
 export default async function SubmissionsServerPage() {
-  const isAuth = (await cookies()).get('auth')?.value === 'true'
-  if (!isAuth) redirect('/home')
   return <SubmissionsPage />
 }
