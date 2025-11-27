7972010d9a96cccfc9331105888257a071283ca0
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2025-11-28 03:16:47 +0530

    feat: update login api route to use JWT tokens instead of plain cookie

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,18 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server'
+import jwt from 'jsonwebtoken'
 import { cookies } from 'next/headers'
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   const { password } = await req.json()
 
-  if (password === process.env.ADMIN_PASSWORD) {
-    ;(await cookies()).set('auth', 'true', {
-      httpOnly: true,
-      path: '/',
-      secure: process.env.NODE_ENV === 'production',
-      maxAge: 60 * 60 * 24,
-    })
-
-    return Response.json({ ok: true })
+  if (!password || password !== process.env.ADMIN_PASSWORD) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
   }
 
-  return new Response('Unauthorized', { status: 401 })
+  const token = jwt.sign({ role: 'admin' }, process.env.JWT_SECRET as string, {
+    // expiresIn: '365d',
+  })
+
+  ;(await cookies()).set('auth', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    path: '/',
+  })
+
+  return NextResponse.json({ success: true })
 }
